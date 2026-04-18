cc7956826c5774c63383197ab2d5b7c717e80714
Alexander Freeman <alexander.freeman@eneco.com>
2026-04-18 13:37:01 +0200

    Update: change images when sound

--- src/ui/signal/signal.gd
+++ src/ui/signal/signal.gd
@@ -4,6 +4,11 @@ signal replay
 
 @onready var replay_btn: Button = $HFlowContainer/Replay
 
+const SPEAKER = preload("uid://copa0m4oo0g2p")
+const SPEAKER_SOUND = preload("uid://cmtgw5gp41fat")
+const SPEAKER_SOUND_2 = preload("uid://oodi5gkxskfm")
+@onready var image: TextureRect = $HFlowContainer/Image
+
 const BOOP = preload("uid://bm86nr26mc1uk")
 const DASH = preload("uid://htguj7msnu46")
 const DOT = preload("uid://c7chjfm0scum5")
@@ -12,7 +17,7 @@ const DOT = preload("uid://c7chjfm0scum5")
 func play_message(message: Array[EncryptedMessage.MorseSignal]):
 	for s in message:
 		if s == EncryptedMessage.MorseSignal.Dot:
-			self.dot()
+			await self.dot()
 		if s == EncryptedMessage.MorseSignal.Dash:
 			self.dash()
 		if s == EncryptedMessage.MorseSignal.Boop:
@@ -26,14 +31,20 @@ func _ready():
 func dot():
 	audio_stream_player.stream = DOT
 	audio_stream_player.play()
+	self.image.texture = SPEAKER_SOUND
 	await audio_stream_player.finished
+	self.image.texture = SPEAKER
 func dash():
 	audio_stream_player.stream = DASH
 	audio_stream_player.play()
+	self.image.texture = SPEAKER_SOUND_2
 	await audio_stream_player.finished
+	self.image.texture = SPEAKER
 func boop():
 	audio_stream_player.stream = BOOP
 	audio_stream_player.play()
+	self.image.texture = SPEAKER_SOUND
 	await audio_stream_player.finished
+	self.image.texture = SPEAKER
 func space():
 	await get_tree().create_timer(0.2).timeout
