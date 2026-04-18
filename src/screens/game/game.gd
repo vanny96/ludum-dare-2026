5590ef5bc36935ccfb3b9bc0068dc1edd0f5fb66
Alexander Freeman <alexander.freeman@eneco.com>
2026-04-18 15:10:32 +0200

    Update: camera system

--- src/screens/game/game.gd
+++ src/screens/game/game.gd
@@ -1,24 +1,41 @@
 extends Node2D
 
-@export var daily_messages: Array[DailyMessages]
+@onready var cameras: Array[Camera2D] = [
+	$Camera1, $Camera2, $Camera3, $Camera4	
+]
+@onready var current_camera_idx: int = 0
+
+
 
 @onready var buttons: MorseButtons = $UI/Buttons
-@onready var signal_emitter: SignalPanel = $UI/Signal
+@onready var signal_emitter: SignalPanel = %Signal
 @onready var computer: Computer = $UI/Computer
 @onready var submit_buttons: SubmitButtons = $UI/SubmitButtons
-@onready var current_message := daily_messages[current_day_idx].messages[current_message_idx]
+@onready var left: Button = $UI/Left
+@onready var right: Button = $UI/Right
 
+@export var daily_messages: Array[DailyMessages]
+@onready var current_message := daily_messages[current_day_idx].messages[current_message_idx]
 var current_day_idx: int = 0
 var current_message_idx: int = 0
 
 var correct_guesses: int = 0
 var wrong_guesses: int = 0
 
+func set_to_camera(i: int):
+	print(i)
+	self.cameras[current_camera_idx % self.cameras.size()].enabled = false
+	self.cameras[i % self.cameras.size()].enabled = true
+	self.current_camera_idx = i
+	
 func _ready() -> void:
-	self.buttons.dot.connect(func(): computer.add_char("."))
-	self.buttons.boop.connect(func(): computer.add_char("|"))
-	self.buttons.dash.connect(func(): computer.add_char("-"))
-	self.buttons.space.connect(func(): computer.add_char(" "))
+	#self.buttons.dot.connect(func(): computer.add_char("."))
+	#self.buttons.boop.connect(func(): computer.add_char("|"))
+	#self.buttons.dash.connect(func(): computer.add_char("-"))
+	#self.buttons.space.connect(func(): computer.add_char(" "))
+	self.left.pressed.connect(func(): self.set_to_camera(self.current_camera_idx - 1))
+	self.right.pressed.connect(func(): self.set_to_camera(self.current_camera_idx + 1))
+	
 	self.signal_emitter.replay.connect(play_message)
 	self.submit_buttons.submit_answer.connect(submit_answer)
 
