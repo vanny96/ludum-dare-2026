2efc6964fde7c3f876df8b5df9fd3082e9c15288
Alexander Freeman <alexander.freeman@eneco.com>
2026-04-18 13:17:26 +0200

    Update: basic setup of signal pane

--- src/ui/signal/signal.gd
+++ src/ui/signal/signal.gd
@@ -1,10 +1,27 @@
 class_name SignalPanel extends PanelContainer
 
+signal replay
+
+const BOOP = preload("uid://bm86nr26mc1uk")
+const DASH = preload("uid://htguj7msnu46")
+const DOT = preload("uid://c7chjfm0scum5")
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+func play_message(message: Array[EncryptedMessage.MorseSignal]):
+	for s in message:
+		print(s)
+
 func dot():
-	pass
+	audio_stream_player.stream = DOT
+	audio_stream_player.play()
+	await audio_stream_player.finished
 func dash():
-	pass
+	audio_stream_player.stream = DASH
+	audio_stream_player.play()
+	await audio_stream_player.finished
 func boop():
-	pass	
+	audio_stream_player.stream = BOOP
+	audio_stream_player.play()
+	await audio_stream_player.finished
 func space():
-	pass	
+	await get_tree().create_timer(0.2).timeout
