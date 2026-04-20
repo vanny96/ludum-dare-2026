f53c7d95037380502533a8704f058b01c0a3cae6
Giovanni Barbaro <vanni-96@hotmail.it>
2026-04-20 13:43:16 +0200

    Sets up audio sliders for music and sfx

--- src/ui/settings/settings.gd
+++ src/ui/settings/settings.gd
@@ -1,20 +1,26 @@
 extends Control
 
 @onready var master_audio_slider: Slider = $PanelContainer/VBoxContainer/GridContainer/MasterAudio/MasterAudio
+@onready var sfx_audio_slider: Slider = $PanelContainer/VBoxContainer/GridContainer/SFX/MasterAudio
+@onready var music_audio_slider: Slider = $PanelContainer/VBoxContainer/GridContainer/Music/MasterAudio
 @onready var sensibility_slider: Slider = $PanelContainer/VBoxContainer/GridContainer/Sensibility/Sensibility
 @onready var close_button: Button = $PanelContainer/VBoxContainer/CloseButton
 
 @onready var master_bus_idx: int = AudioServer.get_bus_index("Master")
+@onready var sfx_bus_idx: int = AudioServer.get_bus_index("SFX")
+@onready var music_bus_idx: int = AudioServer.get_bus_index("Music")
 
 func _ready() -> void:
-	_setup_master_audio()
+	_setup_audio_slider(master_audio_slider, master_bus_idx)
+	_setup_audio_slider(sfx_audio_slider, sfx_bus_idx)
+	_setup_audio_slider(music_audio_slider, music_bus_idx)
 	_setup_sensibility()
 	_setup_close_button()
-
-func _setup_master_audio():
-	master_audio_slider.value = AudioServer.get_bus_volume_linear(master_bus_idx) * 100
-	master_audio_slider.value_changed.connect(func(value): 
-		AudioServer.set_bus_volume_linear(master_bus_idx, value / 100)
+	
+func _setup_audio_slider(slider: Slider, bus_idx: int):
+	slider.value = AudioServer.get_bus_volume_linear(bus_idx) * 100
+	slider.value_changed.connect(func(value): 
+		AudioServer.set_bus_volume_linear(bus_idx, value / 100)
 	)
 	
 func _setup_sensibility():
